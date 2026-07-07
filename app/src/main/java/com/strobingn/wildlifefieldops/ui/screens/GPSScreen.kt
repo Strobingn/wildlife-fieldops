c9c8fadd60f9f2f0c154aa0da47dae2009896dcb
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-07 16:16:47 -0400

    fix: correct MapType import in GPSScreen.kt

--- app/src/main/java/com/strobingn/wildlifefieldops/ui/screens/GPSScreen.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/ui/screens/GPSScreen.kt
@@ -26,7 +26,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 import com.strobingn.wildlifefieldops.ui.theme.*
 import kotlinx.coroutines.delay
-import com.google.maps.android.compose.MapType
+import com.google.android.gms.maps.model.MapType
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
