e1f3167a0d32efa45b50f6209fa2f9a3bcd10f18
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-14 13:42:18 -0400

    Create CatchType.kt - extracted for KSP Room compatibility

---
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/CatchType.kt
@@ -0,0 +1,6 @@
+package com.strobingn.wildlifefieldops.data.model
+
+enum class CatchType {
+    NONE, RACCOON, SQUIRREL, SKUNK, OPOSSUM, BAT, BIRD, SNAKE, RODENT, FOX,
+    COYOTE, GROUNDHOG, CHIPMUNK, BEAVER, OTHER
+}
