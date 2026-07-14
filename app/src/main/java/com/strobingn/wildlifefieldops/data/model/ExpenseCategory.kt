532044bfa39ffca590cb0483db86fb716031b6fc
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-14 13:42:06 -0400

    Create ExpenseCategory.kt - extracted for KSP Room compatibility

---
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/ExpenseCategory.kt
@@ -0,0 +1,6 @@
+package com.strobingn.wildlifefieldops.data.model
+
+enum class ExpenseCategory {
+    FUEL, MATERIALS, TOOLS, PERMITS, LODGING, MEALS, VEHICLE, INSURANCE,
+    MARKETING, OFFICE, SUBCONTRACTOR, EQUIPMENT, OTHER
+}
