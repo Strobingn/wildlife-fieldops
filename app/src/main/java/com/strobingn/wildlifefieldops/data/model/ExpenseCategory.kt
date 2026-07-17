5e0fee1563476f4a803d6b00cef3216c38b03efa
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 12:25:53 -0400

    Fix: Update package name from wildlifefieldops to wildlife

--- app/src/main/java/com/strobingn/wildlifefieldops/data/model/ExpenseCategory.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/ExpenseCategory.kt
@@ -1,4 +1,4 @@
-package com.strobingn.wildlifefieldops.data.model
+package com.strobingn.wildlife.data.model
 
 enum class ExpenseCategory {
     FUEL, MATERIALS, TOOLS, PERMITS, LODGING, MEALS, VEHICLE, INSURANCE,
