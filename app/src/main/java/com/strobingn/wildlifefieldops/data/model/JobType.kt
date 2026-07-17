e23ade270e789cfbd2963cbdc153283d9cf4986d
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 12:26:17 -0400

    Fix: Update package name from wildlifefieldops to wildlife

--- app/src/main/java/com/strobingn/wildlifefieldops/data/model/JobType.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/JobType.kt
@@ -1,4 +1,4 @@
-package com.strobingn.wildlifefieldops.data.model
+package com.strobingn.wildlife.data.model
 
 enum class JobType {
     INSPECTION, REMOVAL, REPAIR, PREVENTION, CLEANUP, CONSULTATION, EMERGENCY,
