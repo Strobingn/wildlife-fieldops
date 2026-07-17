b9f239ddf6b87175c93217c792b6674b5bdeec12
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 11:58:53 -0400

    Fix: Add WorkManager and hilt-work dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.1.0")
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
@@ -93,6 +94,7 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("io.ktor:ktor-client-android:2.3.12")
 
