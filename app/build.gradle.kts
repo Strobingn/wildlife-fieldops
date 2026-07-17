5934531fcdd240a5bc036c9d4c9a4ca1db5fa76d
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 11:58:30 -0400

    Fix: Add hilt-work dependency for @HiltWorker support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.1.0")
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
