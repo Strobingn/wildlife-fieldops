0c2281e7877fcc1e03dcaba62bcced0e06c91d63
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 12:36:27 -0400

    Fix: Revert package to wildlifefieldops to match directory structure

--- app/src/main/java/com/strobingn/wildlifefieldops/data/model/CatchType.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/CatchType.kt
@@ -1,4 +1,4 @@
-package com.strobingn.wildlife.data.model
+package com.strobingn.wildlifefieldops.data.model
 
 enum class CatchType {
     NONE, RACCOON, SQUIRREL, SKUNK, OPOSSUM, BAT, BIRD, SNAKE, RODENT, FOX,
