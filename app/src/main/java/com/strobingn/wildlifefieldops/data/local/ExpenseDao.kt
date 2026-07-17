65eeec0f6761f144c12a5fbfe0ab151ea2e381de
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 12:25:27 -0400

    Fix: Update package name from wildlifefieldops to wildlife

--- app/src/main/java/com/strobingn/wildlifefieldops/data/local/ExpenseDao.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/local/ExpenseDao.kt
@@ -1,7 +1,7 @@
-package com.strobingn.wildlifefieldops.data.local
+package com.strobingn.wildlife.data.local
 
 import androidx.room.*
-import com.strobingn.wildlifefieldops.data.model.Expense
+import com.strobingn.wildlife.data.model.Expense
 import kotlinx.coroutines.flow.Flow
 
 @Dao
