6f02b410bd44d84e923723a6baf588954c5020a7
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-07-17 12:36:35 -0400

    Fix: Revert package to wildlifefieldops to match directory structure

--- app/src/main/java/com/strobingn/wildlifefieldops/data/model/ExpenseCategory.kt
+++ app/src/main/java/com/strobingn/wildlifefieldops/data/model/ExpenseCategory.kt
@@ -1,4 +1,4 @@
-package com.strobingn.wildlife.data.model
+package com.strobingn.wildlifefieldops.data.model
 
 enum class ExpenseCategory {
     FUEL, MATERIALS, TOOLS, PERMITS, LODGING, MEALS, VEHICLE, INSURANCE,
